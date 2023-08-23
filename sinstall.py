0693da17f35a04417d6c86d6fca3804046dae345
mrkiller90 <136891488+mrkiller90@users.noreply.github.com>
2023-08-23 20:27:16 +0330

    Add files via upload

--- sinstall.py
+++ sinstall.py
@@ -34,42 +34,42 @@ def replace_ssh_cipher():
         replace_line(sshd_config_file, pattern, replacement)
         print("SSH encryption configuration completed successfully!")
 
-menu = """
-[1] install BBR 
-[2] install warp
-[3] install security script
-[4] install security script2
-[5] install ssh connection encryption 
-[6] install firewall
-[7] install warp2
-[8] Exit
-"""
+def main_menu():
+    menu = """
+    [1] install BBR 
+    [2] install warp
+    [3] install security script
+    [4] install security script2
+    [5] install ssh connection encryption 
+    [6] install firewall
+    [7] install warp2
+    [8] Exit
+    """
+    
+    # تعریف تمام اسکریپت‌ها در یک لیست
+    all_scripts = [
+        install_BBR,
+        install_warp,
+        install_security_script,
+        install_security_script2,
+        replace_ssh_cipher,
+        add_ports_to_firewall,
+        install_warp2
+    ]
+    
+    while True:
+        print("Welcome To Mrkiller Script ! @Mr_Killer_1\n")
+        print(menu)
+        m = input(">>> ")
 
-print("Welcome To Mrkiller Script ! @Mr_Killer_1\n")
-print(menu)
+        if m == '8':
+            break
+        elif m.isdigit() and 1 <= int(m) <= len(all_scripts):
+            script_index = int(m) - 1
+            selected_script = all_scripts[script_index]
+            selected_script()  # اجرای اسکریپت مربوط به گزینه انتخاب شده
+        else:
+            print("Invalid option. Please select a valid option from the menu.")
 
-while True:
-    m = input(">>> ")
-
-    if m == '1':
-        install_BBR()
-    elif m == '2':
-        install_warp()
-    elif m == '3':
-        subprocess.run(["bash", "-c", "curl -sSL https://raw.githubusercontent.com/elemen3/wepn/master/wepn.sh | bash"])
-    elif m == '4':
-        subprocess.run(["wget", "https://raw.githubusercontent.com/opiran-club/block-iran-ip/main/block-ip.sh"])
-        subprocess.run(["chmod", "+x", "block-ip.sh"])
-        subprocess.run(["./block-ip.sh"])
-    elif m == '5':
-        replace_ssh_cipher()
-    elif m == '6':
-        ports = input("Enter ports (e.g., 8080,9090,7629): ")
-        add_ports_to_firewall(ports)
-        print("Firewall installed successfully!")
-    elif m == '7':
-        subprocess.run(["bash", "-c", "curl -Ls https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh | bash"])
-    elif m == '8':
-        break
-    else:
-        print("Invalid option. Please select a valid option from the menu.")
+if __name__ == "__main__":
+    main_menu()
