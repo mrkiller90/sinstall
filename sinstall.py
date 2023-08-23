7c66ef2f10c23a146cf4d009c15ad4b98f432a7c
mrkiller90 <136891488+mrkiller90@users.noreply.github.com>
2023-08-23 19:11:36 +0330

    Add files via upload

--- sinstall.py
+++ sinstall.py
@@ -1,4 +1,4 @@
-import os
+import subprocess
 import time
 import fileinput
 import re
@@ -6,11 +6,16 @@ import re
 def add_ports_to_firewall(ports):
     ports_list = ports.split(",")
     for port in ports_list:
-        os.system(f"ufw allow {port}/tcp")
+        subprocess.run(["ufw", "allow", f"{port}/tcp"])
 
 # آپدیت
-os.system("apt update -y && apt upgrade -y && apt install curl -y && apt install git -y && apt install screen -y && apt --fix-broken install")
-os.system("clear")
+subprocess.run(["apt", "update", "-y"])
+subprocess.run(["apt", "upgrade", "-y"])
+subprocess.run(["apt", "install", "curl", "-y"])
+subprocess.run(["apt", "install", "git", "-y"])
+subprocess.run(["apt", "install", "screen", "-y"])
+subprocess.run(["apt", "--fix-broken", "install"])
+subprocess.run(["clear"])
 print("Welcome To Mrkiller Script ! @Mr_Killer_1\n")
 
 def banner():
@@ -26,33 +31,36 @@ def banner():
     m = input(">>> ")
 
     if m == '1':
-        os.system("wget -N --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && bash bbr.sh")
+        subprocess.run(["wget", "-N", "--no-check-certificate", "https://github.com/teddysun/across/raw/master/bbr.sh"])
+        subprocess.run(["chmod", "+x", "bbr.sh"])
+        subprocess.run(["bash", "bbr.sh"])
         banner()
     elif m == '2':
-        os.system("bash <(curl -sSL https://raw.githubusercontent.com/hamid-gh98/x-ui-scripts/main/install_warp_proxy.sh)")
+        subprocess.run(["bash", "-c", "curl -sSL https://raw.githubusercontent.com/hamid-gh98/x-ui-scripts/main/install_warp_proxy.sh | bash"])
         banner()
     elif m == '3':
-        os.system("bash <(curl -s https://raw.githubusercontent.com/elemen3/wepn/master/wepn.sh)")
+        subprocess.run(["bash", "-c", "curl -s https://raw.githubusercontent.com/elemen3/wepn/master/wepn.sh | bash"])
         banner()
     elif m == '4':
-        os.system("wget https://raw.githubusercontent.com/opiran-club/block-iran-ip/main/block-ip.sh && chmod +x block-ip.sh && ./block-ip.sh")
+        subprocess.run(["wget", "https://raw.githubusercontent.com/opiran-club/block-iran-ip/main/block-ip.sh"])
+        subprocess.run(["chmod", "+x", "block-ip.sh"])
+        subprocess.run(["./block-ip.sh"])
         banner()
     elif m == '5':
         def replace_ssh_cipher():
             def replace_line(filepath, pattern, replacement):
-                for line in fileinput.input(filepath, inplace=True):
-                    updated_line = re.sub(pattern, replacement, line)
-                    print(updated_line, end='')
+                with fileinput.FileInput(filepath, inplace=True) as file:
+                    for line in file:
+                        updated_line = re.sub(pattern, replacement, line)
+                        print(updated_line, end='')
 
             sshd_config_file = '/etc/ssh/sshd_config'
             pattern = r'^# Ciphers and keying'
             replacement = 'Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes256-ctr'
 
             def check_line(filepath, pattern):
-                for line in fileinput.input(filepath):
-                    if re.search(pattern, line):
-                        return True
-                return False
+                with open(filepath, 'r') as file:
+                    return any(re.search(pattern, line) for line in file)
 
             if not check_line(sshd_config_file, replacement):
                 replace_line(sshd_config_file, pattern, replacement)
@@ -66,7 +74,7 @@ def banner():
         print("Firewall installed successfully!")
         banner()
     elif m == '7':
-        os.system("bash <(curl -Ls https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh)")
+        subprocess.run(["bash", "-c", "curl -Ls https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh | bash"])
         banner()
 
 while True:
